c2956e39a313316e312e437177ef0aa0fb76ecb2
grcthilanjani <140577821+grcthilanjani@users.noreply.github.com>
2023-07-31 18:46:27 +0530

    Add files via upload

---
+++ labsheet2____5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet2___5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            string[] results = new string[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter number " + (i + 1) + ": ");
+                numbers[i] = int.Parse(Console.ReadLine());
+                if (numbers[i] % 2 == 0)
+                {
+                    results[i] = "even";
+                }
+                else
+                {
+                    results[i] = "odd";
+                }
+            }
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Results:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(numbers[i] + " is " + results[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
